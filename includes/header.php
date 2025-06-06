7862ab1d6e802c7d16b9a06128c005490eff7b57
Kristýna Matrasová <kristyna.matrasova@tul.cz>
2025-06-06 18:45:38 +0200

    #01pt.2

--- includes/header.php
+++ includes/header.php
@@ -4,35 +4,31 @@ require_once 'config.php';
 <!DOCTYPE html>
 <html lang="cs">
 <head>
-  <meta charset="UTF-8">
-  <title>Bioinformatický blog</title>
-  <link rel="stylesheet" href="<?= BASE_URL ?>css/style.css">
+<meta charset="UTF-8">
+<title>Bioinformatický blog</title>
+<link rel="stylesheet" href="<?= BASE_URL ?>css/style.css">
 </head>
 <body>
-  <header class="top-bar">
-    <div class="container">
-      <div class="logo">
-        <a href="<?= BASE_URL ?>index.php">
-          <img src="<?= BASE_URL ?>img/logo.png" alt="Logo" class="logo-img">
-          <span class="logo-text">Bioinformatický blog</span>
-        </a>
-      </div>
-      <nav class="main-nav">
-        <ul>
-          <li><a href="<?= BASE_URL ?>index.php">Domů</a></li>
-          <?php if (isset($_SESSION['user_id'])): ?>
-            <li><a href="<?= BASE_URL ?>user/create.php">Přidat článek</a></li>
-            <li><a href="<?= BASE_URL ?>user/logout.php">Odhlásit se (<?= htmlspecialchars($_SESSION['username']) ?>)</a></li>
-          <?php else: ?>
-            <li><a href="<?= BASE_URL ?>user/login.php">Přihlásit se</a></li>
+<header class="top-bar">
+<div class="container">
+<div class="logo">
+<a href="<?= BASE_URL ?>index.php">
+<img src="<?= BASE_URL ?>img/logo.png" alt="Logo" class="logo-img">
+<span class="logo-text">Bioinformatický blog</span>
+</a>
+</div>
+<nav class="main-nav">
+<ul>
+<li><a href="<?= BASE_URL ?>index.php">Domů</a></li>
+<?php if (isset($_SESSION['user_id'])): ?>
+<li><a href="<?= BASE_URL ?>user/create.php">Přidat článek</a></li>
+<li><a href="<?= BASE_URL ?>user/logout.php">Odhlásit se (<?= htmlspecialchars($_SESSION['username']) ?>)</a></li>
+<?php else: ?>
+<li><a href="<?= BASE_URL ?>user/login.php">Přihlásit se</a></li>
             <li><a href="<?= BASE_URL ?>user/register.php">Registrovat</a></li>
-          <?php endif; ?>
-        </ul>
-        <a href="<?= BASE_URL ?>index.php">Domů</a>
-<a href="<?= BASE_URL ?>pages/about.php">O bioinformatice</a>
-<a href="<?= BASE_URL ?>user/login.php">Přihlásit se</a>
-
-      </nav>
-    </div>
-  </header>
-  <main class="main-content">
+<?php endif; ?>
+</ul>
+</nav>
+</div>
+</header>
+<main class="main-content">
